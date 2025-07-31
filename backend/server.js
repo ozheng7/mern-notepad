544f5dda3a6fbafc667162f8e07895f9f121df1d
Oscar Zheng <85961849+ozheng7@users.noreply.github.com>
2025-07-31 14:45:43 -0700

    setup
    - created backend
    - cleaned up js
    - setup nodemon for live updates
    - scripts

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+// NOTES
+// nodemon for live updates to the server, instead of killing and starting again
+
+// common js syntax
+// const express = require("express");
+
+// module syntax
+import express from "express";
+import notesRoutes from "./routes/notesRoutes.js";
+
+const app = express();
+
+// if request starts with this, then use this file
+app.use("/api/notes", notesRoutes);
+
+// listening for this get request @ localhost:5001/api/notes
+
+// listen to the port 5001
+app.listen(5001, () => {
+  console.log("Server started on port 5001");
+});
