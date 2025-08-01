6e2fe08b51d42c923b38b238a132015f0e11d3e4
Oscar Zheng <85961849+ozheng7@users.noreply.github.com>
2025-08-01 10:24:18 -0700

    rate limiter
    - implemented rate limiting using middleware (express)
    - finish building api

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,16 +9,18 @@ import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
+import rateLimiter from "./middleware/ratelimiter.js";
+import { connect } from "mongoose";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-connectDB();
-
 // middleware
 app.use(express.json());
+app.use(rateLimiter);
+// in between the client and the server (req)
 
 // if request starts with this, then use this file
 app.use("/api/notes", notesRoutes);
@@ -26,6 +28,8 @@ app.use("/api/notes", notesRoutes);
 // listening for this get request @ localhost:5001/api/notes
 
 // listen to the port 5001
-app.listen(5001, () => {
-  console.log("Server started on PORT:", PORT);
+connectDB().then(() => {
+  app.listen(5001, () => {
+    console.log("Server started on PORT:", PORT);
+  });
 });
