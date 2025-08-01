338121d5c6e0e02377bb3dedb1d63bc01ded7a6f
Oscar Zheng <85961849+ozheng7@users.noreply.github.com>
2025-07-31 17:38:54 -0700

    note model
    - created note model for database

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,6 +13,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5001
 
 connectDB();
 
@@ -23,5 +24,5 @@ app.use("/api/notes", notesRoutes);
 
 // listen to the port 5001
 app.listen(5001, () => {
-  console.log("Server started on port 5001");
+  console.log("Server started on PORT:", PORT);
 });
