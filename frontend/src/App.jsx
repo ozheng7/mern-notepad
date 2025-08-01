23c0d88ef87103c9f096c4364aff01f9ed476497
Oscar Zheng <85961849+ozheng7@users.noreply.github.com>
2025-08-01 14:57:55 -0700

    frontend setup
    setup with
    - react hot toast
    - tailwind
    - daisyui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,18 @@ import { Route, Routes } from "react-router";
 import HomePage from "./pages/HomePage";
 import CreatePage from "./pages/CreatePage";
 import NoteDetailPage from "./pages/NoteDetailPage";
+import toast from "react-hot-toast";
 
 const App = () => {
   return (
-    <div>
+    <div data-theme="forest">
+      <button className="btn btn-active">Default</button>
+      <button className="btn btn-active btn-neutral">Neutral</button>
+      <button className="btn btn-active btn-primary">Primary</button>
+      <button className="btn btn-active btn-secondary">Secondary</button>
+      <button className="btn btn-active btn-accent">Accent</button>
+      <button className="btn btn-active btn-ghost">Ghost</button>
+      <button className="btn btn-active btn-link">Link</button>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />
